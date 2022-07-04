ea50d95513d55e4f7ed86dec5bd8b87d3cc00539
Mark Akom <mark.akom2019@gmail.com>
2022-07-04 14:08:34 +0000

    Setup a class component for App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
