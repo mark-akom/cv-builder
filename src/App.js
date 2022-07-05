8e28d33594d6184ecae4c9513e703cac6406af2c
Mark Akom <mark.akom2019@gmail.com>
2022-07-05 09:51:43 +0000

    Setup state and function for handling personal info data

--- src/App.js
+++ src/App.js
@@ -5,15 +5,29 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      profileInfo: {
+        fullName: "",
+        email: "",
+        phoneNumber: "",
+        profession: "",
+        profileDesc: ""
+      }
     }
-
+    this.handleProfileChange = this.handleProfileChange.bind(this);
   }
+
+  handleProfileChange(e) {
+    this.setState({
+      profileInfo: { ...this.state.profileInfo, [e.target.id]: e.target.value }
+    })
+  }
+
   render() {
+    const { profileInfo } = this.state;
     return (
       <div>
         <h1>Online CV Builder</h1>
-        <PersonalInfo />
+        <PersonalInfo profile={profileInfo} handleChange={this.handleProfileChange} />
       </div>
     )
   }
