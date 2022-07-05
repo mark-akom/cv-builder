786be2fa49696b248f2371d24481887a2cdf6d51
Mark Akom <mark.akom2019@gmail.com>
2022-07-05 09:52:19 +0000

    Setup form to take input from user

---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+class PersonalInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        const { profile, handleChange } = this.props;
+        return (
+            <div className="personal-details-container">
+                <form>
+                    <div className="name-sec">
+                        <label htmlFor="fullName">Full Name:</label>
+                        <input type="text" id="fullName" name="fullname" onChange={(e) => handleChange(e)} value={profile.fullName} />
+                    </div>
+                    <div className="profession-sec">
+                        <label htmlFor="profession">Profession:</label>
+                        <input type="text" id="profession" name="user-profession" onChange={(e) => handleChange(e)} value={profile.profession} />
+                    </div>
+                    <div className="profile-desc-sec">
+                        <label htmlFor="profileDesc">Profile Description:</label>
+                        <textarea value={profile.profileDesc} id="profileDesc" onChange={(e) => handleChange(e)} name="user-profile-desc"></textarea>
+                    </div>
+                    <div className="email-sec">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="user-email" onChange={(e) => handleChange(e)} value={profile.email} />
+                    </div>
+                    <div className="phone-sec">
+                        <label htmlFor="phoneNumber">Phone Number:</label>
+                        <input type="text" id="phoneNumber" onChange={(e) => handleChange(e)} name="user-phone-number" value={profile.phoneNumber} />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default PersonalInfo
