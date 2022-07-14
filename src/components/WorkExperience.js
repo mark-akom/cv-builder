eea62b5869560606a81f6ad8aa7b37219d735151
Mark Akom <mark.akom2019@gmail.com>
2022-07-14 13:04:24 +0000

    add onchange handler for text fields

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,29 +1,29 @@
 import React from "react";
 
-const WorkExperience = ({workExperience}) => {
+const WorkExperience = ({workExperience, handleWorkExperienceChange}) => {
 
     return (
         <div className="work-experience-section">
             <form>
                 <div className="company-name-sec">
                     <label htmlFor="companyName">Company Name:</label>
-                    <input type="text" id="companyName" value={workExperience.companyName} name="user-company-name" />
+                    <input type="text" id="companyName" onChange={(e) => handleWorkExperienceChange(e, workExperience.id)} value={workExperience.companyName} name="user-company-name" />
                 </div>
                 <div className="position-sec">
                     <label htmlFor="position">Position:</label>
-                    <input type="text" id="position" value={workExperience.position} name="user-position" />
+                    <input type="text" id="position" onChange={(e) => handleWorkExperienceChange(e, workExperience.id)} value={workExperience.position} name="user-position" />
                 </div>
                 <div className="start-date-sec">
                     <label htmlFor="startDate">Start Date:</label>
-                    <input type="text" id="startDate" value={workExperience.startDate} name="user-start-date" />
+                    <input type="text" id="startDate" onChange={(e) => handleWorkExperienceChange(e, workExperience.id)} value={workExperience.startDate} name="user-start-date" />
                 </div>
                 <div className="end-date-sec">
                     <label htmlFor="endDate">End Date:</label>
-                    <input type="text" id="endDate" value={workExperience.endDate} name="user-end-date" />
+                    <input type="text" id="endDate" onChange={(e) => handleWorkExperienceChange(e, workExperience.id)} value={workExperience.endDate} name="user-end-date" />
                 </div>
                 <div>
                     <label htmlFor="jobRole">Job Role</label>
-                    <textarea id="jobRole" value={workExperience.jobRole} name="user-job-role"></textarea>
+                    <textarea id="jobRole" onChange={(e) => handleWorkExperienceChange(e, workExperience.id)} value={workExperience.jobRole} name="user-job-role"></textarea>
                 </div>
             </form>
         </div>
